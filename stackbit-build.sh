46a5615bd35088acee3bd219797ce7a8cb4765c1
Stackbit <projects@stackbit.com>
2021-02-21 11:05:35 +0000

    initial commit by Stackbit

---
+++ stackbit-build.sh
@@ -0,0 +1,15 @@
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+
+***REMOVED***
+npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/60323df295aa610015b59f66
+
+***REMOVED***
+npm run build
+
+***REMOVED***
