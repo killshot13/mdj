40365acc28f3847061b2539c569a347a782a954c
Michael Rehnert (killshot13) <dmreh@outlook.com>
2021-09-01 05:01:57 -0500

    Update(mdj): restructured npm packages

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -7,9 +7,9 @@
 ***REMOVED***
 
 ***REMOVED***
-npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/60323df295aa610015b59f66 --stackbit-api-key="bd48a94d051a6ed548c6627e74e2075f911d90abe3b7ded6ca5f0249aa5ce996"
+***REMOVED***
 
 ***REMOVED***
-npm run build
+***REMOVED***
 
 ***REMOVED***
